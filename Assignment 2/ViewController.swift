80101dae52bffbeae0d2b9fb7f602d4174725975
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-02-05 14:50:46 +0530

    Initial Commit

---
+++ Assignment 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by MSCIT on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
