1d8328e43264d07997c43572c0f221131971cc89
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-02-07 02:31:07 -0800

    Q4 controller added

---
+++ Assignment 2/Q4-(1).swift
@@ -0,0 +1,17 @@
+//
+//  Q4-(1).swift
+//  Assignment 2
+//
+//  Created by HPG on 07/02/24.
+//
+
+import UIKit
+
+class Q4__1_: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Assignment 2/Q4-(2).swift
@@ -0,0 +1,17 @@
+//
+//  Q4-(2).swift
+//  Assignment 2
+//
+//  Created by HPG on 07/02/24.
+//
+
+import UIKit
+
+class Q4__2_: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
