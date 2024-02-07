ab547ecdf11e12d34c17ee067458d3eb5ce27b58
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-02-07 01:33:01 -0800

    2nd Question pending

---
+++ Assignment 2/Q2-(1).swift
@@ -0,0 +1,28 @@
+//
+//  Q2-(1).swift
+//  Assignment 2
+//
+//  Created by HPG on 07/02/24.
+//
+
+import UIKit
+
+class Q2__1_: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    var data:String = "";
+    
+    @IBOutlet weak var t1box: UITextField!
+    @IBOutlet weak var t2box: UITextField!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let obj = segue.description as! Q2__2_
+        obj.str = self.data;
+    }
+    @IBAction func btnclick(_ sender: Any) {
+        data = "\(t1box.text) | \(t2box.text)";
+    }
+}
