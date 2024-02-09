67fed83a9db1c2d663ff72afd2a6de810110de46
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-02-08 21:42:15 -0800

    button click logic implemented

--- Assignment 2/Q3.swift
+++ Assignment 2/Q3.swift
@@ -17,35 +17,50 @@ class Q3: UIViewController {
     @IBOutlet weak var inputScreenlbl: UITextField!
     
     @IBAction func b1click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)1"
     }
     @IBAction func b2click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)2"
     }
     @IBAction func b3click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)3"
     }
     @IBAction func b4click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)4"
     }
     @IBAction func b5click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)5"
     }
     @IBAction func b6click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)6"
     }
     @IBAction func b7click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)7"
     }
     @IBAction func b8click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)8"
     }
     @IBAction func b9click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)9"
     }
     @IBAction func b0click(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!)0"
     }
     @IBAction func sumbtn(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!) + "
     }
     @IBAction func subbtn(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!) - "
     }
     @IBAction func mulbtn(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!) * "
     }
     @IBAction func divbtn(_ sender: Any) {
+        inputScreenlbl.text = "\(inputScreenlbl.text!) / "
     }
     @IBAction func ansbtn(_ sender: Any) {
     }
     @IBAction func clearbtn(_ sender: Any) {
+        inputScreenlbl.text = ""
     }
 }
